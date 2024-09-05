8d9fc14b50157eb0a79440e46b412c29f69e5dd2
Thibault Apretna <thibault.apretna@gmail.com>
2024-09-05 18:10:21 +0200

    ajout des requêtes utilisateurs pour le Signup et le Login

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const signUp = require("../utilitaryFunctions/signUp");
+const login = require("../utilitaryFunctions/login");
+const update = require("../utilitaryFunctions/update");
+const isAuthenticated = require("../middlewares/isAutenticated");
 router.use(express.json());
+const axios = require("axios");
+
+router.post("/signup", signUp, async (req, res) => {
+  console.log(req);
+  res.status(201).json({
+    _id: req.body._id,
+    token: req.body.token,
+    username: req.body.username,
+  });
+});
+
+router.post("/login", login);
+
+router.put("/update/:id", update, (req, res) => {
+  res.status(200).json({ message: "Le profil a bien été mis à jour" });
+});
 
 module.exports = router;
